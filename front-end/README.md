a0e5710990d29cb0d96f5cb7deb0e3465ddd67fa
elminsterrr <przemyslaw.glab@gmail.com>
2018-04-04 17:54:26 +0200

    First front-end commit

---
+++ front-end/README.md
@@ -0,0 +1,27 @@
+# guess-number
+
+## Getting Started
+
+### Installation
+
+Open a Terminal and run:
+
+```sh
+npm install
+```
+
+### Usage
+
+Start the app by typing:
+
+```sh
+npm start
+```
+
+### Testing
+
+Start testing by typing:
+
+```sh
+npm run test
+```
---
+++ front-end/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App-container">
+        <h3>The Secret Number Finder</h3>
+      </div>
+    )
+  }
+}
+
+export default App;
