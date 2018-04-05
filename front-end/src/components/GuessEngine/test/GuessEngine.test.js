884524578b7ab84591c0754762a910c8743dc16d
elminsterrr <przemyslaw.glab@gmail.com>
2018-04-05 03:10:50 +0200

    Add some initial tests

--- front-end/src/components/GuessEngine/test/GuessEngine.test.js
+++ front-end/src/components/GuessEngine/test/GuessEngine.test.js
@@ -8,4 +8,20 @@ describe('GuessEngine', () => {
   it('renders correctly', () => {
     expect(guessEngine).toMatchSnapshot();
   });
+
+  it('initializes the `state` with result property', () => {
+    expect(guessEngine.state().result).toEqual(null);
+  });
+
+  it('initializes the `state` with number property', () => {
+    expect(guessEngine.state().number).toEqual(null);
+  });
+
+  it('initializes the `state` with an empty array of guesses', () => {
+    expect(guessEngine.state().guesses).toEqual([]);
+  });
+
+  it('initializes the `state` with error property', () => {
+    expect(guessEngine.state().error).toEqual(null);
+  });
 });
